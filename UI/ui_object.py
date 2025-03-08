add3a23c94ec4168f2d09458acd3d272f34ca1d1
clementlaf <clafond8@gmail.com>
2025-03-08 15:30:56 +0100

    Refactor UiObject and Builder classes for improved parameter handling and drawing functionality

--- UI/ui_object.py
+++ UI/ui_object.py
@@ -1,25 +1,41 @@
+"""This module contains the UiObject class,
+ which is the base class for all the UI objects in the UI module"""
+
 import pygame as pg
-from vector import Vector as vec
+from UI.vector import Vector as vec
+from UI.link import get
 
 class Builder:
     def __init__(self):
         self.params = {
-            "pos_type": "relative",
-            "background_color": None,
-            "border_color": None,
-            "border_width": 0,
-            "border_radius": 0
+            "pos_type": "relative",   # relative, absolute
+            "background_color": None, # None, color
+            "border_color": None,     # None, color
+            "border_width": 0,        # 0, int
+            "border_radius": 0        # 0, int, [int, int, int, int]
         }
 
-    def __setattr__(self, name, value):
-        self.params[name] = value
+    def __getitem__(self, key: str):
+        return self.params[key]
+    def __setitem__(self, key: str, value):
+        self.params[key] = value
+
+    @property
+    def items(self)-> list:
+        """Return the items of the builder
+
+        Returns:
+            list: The items of the builder
+        """
+
+        return self.params.items()
 
 
 class UiObject:
     def __init__(self, pos: vec, size: vec, builder: Builder):
         self.pos = pos
         self.size = size
-        for key, value in builder.items():
+        for key, value in builder.items:
             setattr(self, key, value)
 
     def draw(self, screen: pg.Surface, position: vec):
@@ -30,7 +46,10 @@ class UiObject:
             position (vec): _description_
         """
 
-        pass
+        if self.background_color is not None:
+            pg.draw.rect(screen, self.background_color, pg.Rect(get(position).to_tuple(), get(self.size).to_tuple()), 0, self.border_radius)
+        if self.border_width != 0 and self.border_color is not None:
+            pg.draw.rect(screen, self.border_color, pg.Rect(get(position).to_tuple(), get(self.size).to_tuple()), self.border_width, self.border_radius)
 
     def is_under_point(self, position: vec, point: vec)-> bool:
         """Check if the point is under the object
@@ -43,8 +62,8 @@ class UiObject:
             bool: True if the point is under the object, False otherwise
         """
 
-        rect = pg.Rect(position.to_tuple(), self.size.to_tuple())
-        return rect.collidepoint(point.to_tuple())
+        rect = pg.Rect(get(position).to_tuple(), get(self.size).to_tuple())
+        return rect.collidepoint(get(point).to_tuple())
 
     def is_under_rect(self, position: vec, rect: pg.Rect)-> bool:
         """Check if the object is under the rect
@@ -57,7 +76,7 @@ class UiObject:
             bool: True if the object is under the rect, False otherwise
         """
 
-        return pg.Rect(position.to_tuple(), self.size.to_tuple()).colliderect(rect)
+        return pg.Rect(get(position).to_tuple(), get(self.size).to_tuple()).colliderect(rect)
 
     def intersection(self, position: vec, rect: pg.Rect)-> pg.Rect:
         """Return the intersection between the object and the rect
@@ -70,4 +89,4 @@ class UiObject:
             pg.Rect: The intersection between the object and the rect 
         """
 
-        return pg.Rect(position.to_tuple(), self.size.to_tuple()).clip(rect)
+        return pg.Rect(get(position).to_tuple(), get(self.size).to_tuple()).clip(rect)
