083cbc2406a39eb7f729ccbca71b3c5216264333
clementlaf <clafond8@gmail.com>
2025-03-22 22:06:54 +0100

    Add Inconsolata variable font to AppState initialization

--- app_state.py
+++ app_state.py
@@ -3,3 +3,4 @@ class AppState:
     def __init__(self):
         self.screen_size = (800, 600)
         self.background_color = (0, 0, 0)
+        self.text_font = "data/fonts/Inconsolata-VariableFont_wdth,wght.ttf"
