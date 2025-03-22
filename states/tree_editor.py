8b2a53b297e4773f393246a17731b50a5079a518
clementlaf <clafond8@gmail.com>
2025-03-23 00:11:46 +0100

    Add name parameter to Widget instantiation in TreeEditor

--- states/tree_editor.py
+++ states/tree_editor.py
@@ -7,4 +7,4 @@ class TreeEditor(BaseState):
         super().__init__(app)
 
         top_right_pos_link = LinkByMethod(self.app_state, lambda x: (x.screen_size[0] - 300, 0))
-        self.ui_context.add_widget(Widget(top_right_pos_link, (300, 50), on_click=self.app.quit, background_color=(255, 0, 0)))
+        self.ui_context.add_widget(Widget(top_right_pos_link, (300, 50), "yup", on_click=self.app.quit, background_color=(255, 0, 0)))
