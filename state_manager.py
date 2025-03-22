bbdc1ece3d3dfdc06d39136f2fc7332066b1cd30
clementlaf <clafond8@gmail.com>
2025-03-22 22:07:12 +0100

    Refactor StateManager to rename state variable for clarity

--- state_manager.py
+++ state_manager.py
@@ -1,19 +1,19 @@
 class StateManager:
     def __init__(self, app):
         self.app = app
-        self.state = None
+        self.crt_state = None
 
     def change_state(self, new_state):
-        self.state = new_state
+        self.crt_state = new_state
 
     def handle_events(self):
-        if self.state:
-            self.state.handle_events()
+        if self.crt_state:
+            self.crt_state.handle_events()
 
     def update(self):
-        if self.state:
-            self.state.update()
+        if self.crt_state:
+            self.crt_state.update()
 
     def draw(self, screen):
-        if self.state:
-            self.state.draw(screen)
+        if self.crt_state:
+            self.crt_state.draw(screen)
