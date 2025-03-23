c933c69f2d10b643418a98e393f89c52cf1f77e1
clementlaf <clafond8@gmail.com>
2025-03-23 13:10:36 +0100

    Refactor state management in main.py to add MainMenu state and set it as active

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from app import App
-from states.main_menu import MainMenu
+from myapp.screens.main_menu import MainMenu
 
 if __name__ == "__main__":
     app = App()
-    app.state_manager.change_state(MainMenu(app))
+    app.state_manager.add_state(MainMenu(app))
+    app.state_manager.set_active(0)
     app.run()
