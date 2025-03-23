926d16e1642a73fceddd645660c8dfeaade99f1b
clementlaf <clafond8@gmail.com>
2025-03-23 13:10:42 +0100

    Refactor StateManager to manage opened states and set active state by index

--- state_manager.py
+++ state_manager.py
@@ -2,18 +2,21 @@ class StateManager:
     def __init__(self, app):
         self.app = app
         self.crt_state = None
+        self.opened_states = []
 
-    def change_state(self, new_state):
-        self.crt_state = new_state
+    def set_active(self, state_idx):
+        self.crt_state = state_idx
+    def add_state(self, state):
+        self.opened_states.append(state)
 
     def handle_events(self):
-        if self.crt_state:
-            self.crt_state.handle_events()
+        if self.crt_state is not None:
+            self.opened_states[self.crt_state].handle_events()
 
     def update(self):
-        if self.crt_state:
-            self.crt_state.update()
+        if self.crt_state is not None:
+            self.opened_states[self.crt_state].update()
 
     def draw(self, screen):
-        if self.crt_state:
-            self.crt_state.draw(screen)
+        if self.crt_state is not None:
+            self.opened_states[self.crt_state].draw(screen)
