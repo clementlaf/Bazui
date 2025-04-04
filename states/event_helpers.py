05be570ac3b14d125817df26e266fbd761bd98bf
clementlaf <clafond8@gmail.com>
2025-04-04 21:04:37 +0200

    Add event helper functions to consume events and prevent text selection

--- states/event_helpers.py
+++ states/event_helpers.py
@@ -1,3 +1,11 @@
 
 def consume_event(widget):
+    """used to consume event on action on a widget
+    """
+
     return True
+
+def no_selection():
+    """used to prevent text selection in text widgets
+    """
+    return None
