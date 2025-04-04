24c2dbbc642b14d0de6be73bdc2d735aaef966b3
clementlaf <clafond8@gmail.com>
2025-04-04 23:27:28 +0200

    Add close_crt_state method to StateManager for state closure handling

--- state_manager.py
+++ state_manager.py
@@ -8,6 +8,14 @@ class StateManager:
         self.crt_state = state_idx
         self.opened_states[state_idx].ui_context.exit_events = True
         self.opened_states[state_idx].on_activation()
+    def close_crt_state(self):
+        if self.crt_state is not None:
+            self.opened_states[self.crt_state].ui_context.exit_events = True
+            self.opened_states.pop(self.crt_state)
+            if len(self.opened_states) > 0:
+                self.set_active(min(self.crt_state, len(self.opened_states) - 1))
+            else:
+                self.app.quit()
     def add_state(self, state):
         self.opened_states.append(state)
 
