783bd835efcdf41a7e803a3e575806fa9b3b82ce
clementlaf <clafond8@gmail.com>
2025-04-05 18:06:52 +0200

    refactor: Clean up AppState initialization and remove unused pygame references

--- bazui/app_state.py
+++ bazui/app_state.py
@@ -1,12 +1,10 @@
-import pygame
 
 class AppState:
-    """Global application state."""
+    """Global application state. Defaults here."""
     def __init__(self):
         self.screen_size = (800, 600)
         self.background_color = "#1E1E1E"
-        self.text_font = "data/fonts/Inconsolata-VariableFont_wdth,wght.ttf"
-        self.test_image = pygame.image.load("d:/medias/photos/portable2/1739303837285.jpg")
+        self.text_font = None
 
         # parameters
         self.undo_stack_max_size = 20
@@ -15,7 +13,3 @@ class AppState:
         # states
         self.dragged_widget = None
         self.mouse_pos = (0, 0)
-
-        # size-defined fonts
-        self.title1f = pygame.font.Font(self.text_font, 50)
-        self.text1f = pygame.font.Font(self.text_font, 20)
