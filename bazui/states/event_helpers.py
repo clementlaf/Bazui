dbdb449433477c099d25427b96dc3fff6ae4cc3b
clementlaf <clafond8@gmail.com>
2025-04-06 13:12:32 +0200

    fix: Add widget spawning and despawning functions for checkboxes

--- bazui/states/event_helpers.py
+++ bazui/states/event_helpers.py
@@ -9,3 +9,20 @@ def no_selection():
     """used to prevent text selection in text widgets
     """
     return None
+
+def spawn_widget_func(spawned_widget):
+    """used to spawn a widget on click
+    """
+    return lambda widget: widget.set_child(spawned_widget)
+def despawn_widget_func(spawned_widget):
+    """used to despawn a widget on click
+    """
+    return lambda widget: widget.remove_child(spawned_widget)
+def deselect_and_despawn_widget_func(spawned_widget):
+    """used to deselect and despawn a widget on click
+    """
+    def myfunc(widget):
+        widget.remove_child(spawned_widget)
+        widget.checked = False
+        return True
+    return myfunc
