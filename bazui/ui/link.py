b9c2e101daa75cc2bdbca46aa9e3579e64ebc2f7
lafondc <clement.lafond@meteo.fr>
2025-04-15 11:49:45 +0200

    feat: Implement caching of links for highly reduced calls per frame

--- bazui/ui/link.py
+++ bazui/ui/link.py
@@ -4,20 +4,34 @@ class Link:
         pass
 
 class LinkAttribute(Link):
-    def __init__(self, ref, attr):
+    def __init__(self, ref, attr, app):
+        self._last_frame = -1
+        self._cached_value = None
         self.ref = ref
         self.attr = attr
+        self.app = app
 
     def get(self):
-        return get(getattr(self.ref, self.attr))
+        if self._last_frame == self.app.frame_id:
+            return self._cached_value
+        self._cached_value = get(getattr(self.ref, self.attr))
+        self._last_frame = self.app.frame_id
+        return self._cached_value
 
 class LinkByMethod(Link):
-    def __init__(self, ref, method):
+    def __init__(self, ref, method, app):
+        self._last_frame = -1
+        self._cached_value = None
         self.ref = ref
         self.method = method
+        self.app = app
 
     def get(self):
-        return get(self.method(self.ref))
+        if self._last_frame == self.app.frame_id:
+            return self._cached_value
+        self._cached_value = get(self.method(self.ref))
+        self._last_frame = self.app.frame_id
+        return self._cached_value
 
 
 
