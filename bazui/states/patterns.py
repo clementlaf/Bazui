0d18cbf568704384324a35d816e71e4b58a5f40e
clementlaf <clafond8@gmail.com>
2025-04-15 16:29:06 +0200

    fix: Add spacing parameter to horizontal and vertical adjustment functions

--- bazui/states/patterns.py
+++ bazui/states/patterns.py
@@ -1,21 +1,21 @@
 from bazui.ui.link import LinkByMethod as LM, get
 
-def horizontal_adjust(widgets, start_pos):
+def horizontal_adjust(widgets, start_pos, spacing=0):
     app = widgets[0].app
     for i, widget in enumerate(widgets):
         if i == 0:
             widget.pos = start_pos
         else:
             prev_widget = widgets[i - 1]
-            widget_pos_link = LM(prev_widget, lambda x, prev_widget=prev_widget: (get(x.pos)[0] + get(prev_widget.size)[0], get(x.pos)[1]), app)
+            widget_pos_link = LM(prev_widget, lambda x, prev_widget=prev_widget: (get(x.pos)[0] + get(prev_widget.size)[0] + spacing, get(x.pos)[1]), app)
             widget.pos = widget_pos_link
 
-def vertical_adjust(widgets, start_pos):
+def vertical_adjust(widgets, start_pos, spacing=0):
     app = widgets[0].app
     for i, widget in enumerate(widgets):
         if i == 0:
             widget.pos = start_pos
         else:
             prev_widget = widgets[i - 1]
-            widget_pos_link = LM(prev_widget, lambda x, prev_widget=prev_widget: (get(x.pos)[0], get(x.pos)[1] + get(prev_widget.size)[1]), app)
+            widget_pos_link = LM(prev_widget, lambda x, prev_widget=prev_widget: (get(x.pos)[0], get(x.pos)[1] + get(prev_widget.size)[1] + spacing), app)
             widget.pos = widget_pos_link
