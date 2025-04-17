f76a8afec37d14126079d850d8d955a45d9a1c5d
clementlaf <clafond8@gmail.com>
2025-04-17 12:43:52 +0200

    fix: Refactor event handling to ensure proper consumption by widgets

--- bazui/states/base_state.py
+++ bazui/states/base_state.py
@@ -1,17 +1,23 @@
 import pygame
 from bazui.ui.ui_context import UIContext
+from bazui.states.hotkey_manager import HotkeyManager
 
 class BaseState:
     def __init__(self, app):
         self.app = app
         self.app_state = app.app_state  # Access shared state
         self.ui_context = UIContext(self.app_state)  # Access UI manager
+        self.hotkeys = HotkeyManager() # Access hotkey manager
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.app.quit(None)
-            self.ui_context.handle_events(event)
+            res = self.ui_context.handle_events(event)
+            if res:
+                # If the event was consumed by a widget, break the loop
+                break
+            self.hotkeys.handle_events(event)
 
     def update(self):
         self.ui_context.update()
